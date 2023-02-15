b67c336247608dd4c539e2fc95d7c0117f2203af
brandonmdee <brandondickerson@cox.net>
2023-02-15 08:09:12 -0600

    Added dummy layout

---
+++ script.js
@@ -0,0 +1,20 @@
+const etch = document.querySelector('.etch');
+const square = document.querySelector('#square');
+const row = document.querySelector('.row');
+const grid = document.querySelector('.grid');
+
+let x = 1; // in this instance, x is the number on the slider to create an x / x grid, so 1/1 here.
+
+//this function will reset the grid by deleting the squares, immediately generating the new size afterward.
+const squares = function () {
+    //etch.removeChild(grid);
+
+}
+/*square.addEventListener('mousedown', () => {
+    square.classList.add('filled');
+    console.log('hello');
+} ) */
+console.log('hey');
+const buttons = document.querySelectorAll('#button');
+
+const blocks = square.forEach('#square');
